b645c62e4e8e1b375202ac5b00ae5997f02fbff3
GHOST <ghostdevbusiness@gmail.com>
2021-09-13 17:22:22 +0100

    feat: gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+import gulpSass from 'gulp-sass';
+import cssnano from 'cssnano';
+import dartSass from 'sass';
+import gulp from 'gulp';
+
+const sass = gulpSass(dartSass);
+
+const plugins = [
+    cssnano(),
+    autoprefixer({ overrideBrowserslist: ['defaults and last 4 version'] }),
+];
+
+gulp.task('build', () =>
+    gulp
+        .src('./scss/ghostsui.scss')
+        .pipe(sass())
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./css')),
+);
+
+gulp.task('dev', () => gulp.watch('./scss/**.scss', gulp.series('build')));
