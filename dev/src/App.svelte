fb280f9073a982af84149312b84d51666dbf9b7a
GHOST <ghostdevbusiness@gmail.com>
2021-09-16 14:41:36 +0100

    chore: update

--- dev/src/App.svelte
+++ dev/src/App.svelte
@@ -1,1 +1,37 @@
-Hello
+<banner class="red">
+    <p>Hello World!</p>
+</banner>
+
+<div class="column" style="padding: 8px;">
+    <div>
+        <h1>Heading One</h1>
+
+        <h2>Heading Two</h2>
+
+        <h3>Heading Three</h3>
+
+        <h4>Heading Four</h4>
+
+        <h5>Heading Five</h5>
+
+        <h6>Heading Six</h6>
+
+        <p>Paragraph</p>
+    </div>
+
+    <div class="row">
+        <h4>Hello</h4>
+
+        <h4>World</h4>
+    </div>
+
+    <a href="https://ghostdev.xyz"> View awesome website </a>
+
+    <a href="https://ghostdev.xyz" role="button"> Anchor styled as button </a>
+
+    <button> Click me! </button>
+
+    <card> Hello World! </card>
+
+    <input type="text" placeholder="Type..." />
+</div>
