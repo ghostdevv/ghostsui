48494a4f11afa79e8342fa27238a7229bfb2f5d5
Willow (GHOST) <git@willow.sh>
2025-11-19 19:28:09 +0000

    feat: add icon-chevron var

--- packages/ghostsui/scripts/build.sh
+++ packages/ghostsui/scripts/build.sh
@@ -1,6 +1,6 @@
 compile_and_minify() {
     echo Processing \"$1\"
-    sass src/$1.scss --no-source-map --load-path=node_modules | pnpm esbuild --loader=css --bundle --minify --outfile=$1.css
+    sass src/$1.scss --no-source-map --load-path=node_modules | pnpm esbuild --loader=css --loader:.svg=dataurl --bundle --minify --outfile=$1.css
 }
 
 compile_and_minify ghostsui
