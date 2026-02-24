69ec11c7e2160503119c14ffdbd12ef097592c3d
Willow (GHOST) <git@willow.sh>
2026-02-24 07:19:50 +0000

    docs: more boilerplatey stuff

--- packages/docs/src/routes/+layout.svelte
+++ packages/docs/src/routes/+layout.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/state';
 	import 'ghostsui';
 
-	let { children } = $props();
+	const { children } = $props();
+
+	const title = $derived.by(() => {
+		switch (page.route.id) {
+			case '/':
+				return 'ghostsui';
+		}
+	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>{title}</title>
 </svelte:head>
 
 {@render children()}
--- packages/docs/src/routes/+page.svelte
+++ packages/docs/src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-	documentation
-</p>
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
+		the documentation
+	</p>
+</main>
